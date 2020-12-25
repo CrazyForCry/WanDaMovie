e30beb47e56ee3f5cfb4c3dc616e5f87d2f3ad67
CrazyForCry <1290334730@qq.com>
2020-12-26 02:28:48 +0800

    ver1.0

---
+++ Movie/js/data.js
@@ -0,0 +1,172 @@
+let data = {
+	imgData: [{
+			id: 1,
+			imgSrc: "./img/cut_picture (39).jpg",
+			title: "温暖的圣诞",
+			time: "14天后结束",
+			num: "80299人关注"
+		},
+		{
+			id: 2,
+			imgSrc: "./img/cut_picture (47).jpg",
+			title: "看万达PRIME赢开年锦鲤",
+			time: "14天后结束",
+			num: "56776人关注"
+		},
+		{
+			id: 3,
+			imgSrc: "./img/cut_picture (45).jpg",
+			title: "扫一扫赢福袋",
+			time: "26天后结束",
+			num: "8180人关注"
+		},
+		{
+			id: 4,
+			imgSrc: "./img/cut_picture (49).jpg",
+			title: "万达电影随心看",
+			time: "42天后结束",
+			num: "163649人关注"
+		},
+		{
+			id: 5,
+			imgSrc: "./img/cut_picture (46).jpg",
+			title: "领取你的专属票根",
+			time: "42天后结束",
+			num: "269810人关注"
+		},
+		{
+			id: 6,
+			imgSrc: "./img/cut_picture(1).png",
+			title: "顾客满意度调查",
+			time: "仅剩30天",
+			num: "74306人关注"
+		},
+		{
+			id: 7,
+			imgSrc: "./img/cut_picture (40).jpg",
+			title: "邀请新人看电影",
+			time: "11天后结束",
+			num: "283492人关注"
+		},
+		{
+			id: 8,
+			imgSrc: "./img/cut_picture (41).jpg",
+			title: "万达电影用户调研问卷",
+			time: "14天后结束",
+			num: "11588人关注"
+		},
+		{
+			id: 9,
+			imgSrc: "./img/cut_picture (43).jpg",
+			title: "周二会员日",
+			time: "11天后结束",
+			num: "50399人关注"
+		},
+		{
+			id: 10,
+			imgSrc: "./img/cut_picture.png",
+			title: "12月签到享好礼",
+			time: "11天后结束",
+			num: "117884人关注"
+		},
+		{
+			id: 11,
+			imgSrc: "./img/cut_picture (44).jpg",
+			title: "soul有所属 暖心好礼",
+			time: "16天后结束",
+			num: "4123人关注"
+		}
+	],
+	movieListData: [{
+			id: 1,
+			img: "./img/cut_picture (53).jpg",
+			title: "紧急救援",
+			classification: "9.1",
+			descp: "'红海''湄公河'金牌班底全新放映",
+			actor: "林超贤/彭于晏/王彦霖",
+			type1: "IMAX",
+			type2: "2D",
+			type3: "CINITY2D",
+			buy: "购票",
+			classType: "buyTypeOne"
+		},
+		{
+			id: 2,
+			img: "./img/cut_picture (6).jpg",
+			title: "神奇女侠1984",
+			classification: "8.0",
+			descp: "动作/冒险/奇幻",
+			actor: "派帝·杰金斯/盖尔·加朵/",
+			type1: "IMAX3D",
+			type2: "CINITY3D",
+			type3: "3D",
+			buy: "购票",
+			classType: "buyTypeOne"
+		},
+		{
+			id: 3,
+			img: "./img/cut_picture (10).jpg",
+			title: "拆弹专家2",
+			classification: "9.1",
+			descp: "2020年12月24日上映",
+			actor: "邱礼涛/刘德华/刘青云",
+			type1: "IMAX",
+			type2: "2D",
+			type3: "CINITY2D",
+			buy: "预售",
+			classType: "buyTypeTwo"
+		},
+		{
+			id: 4,
+			img: "./img/cut_picture (3).jpg",
+			title: "晴雅集",
+			classification: "9.1",
+			descp: "2020年12月25日上映",
+			actor: "郭敬明/赵又廷/邓伦/孙晨竣",
+			type1: "IMAX",
+			type2: "2D",
+			type3: "CINITY2D",
+			buy: "预售",
+			classType: "buyTypeTwo"
+		},
+		{
+			id: 5,
+			img: "./img/cut_picture (1).jpg",
+			title: "沐浴之王",
+			classification: "9.1",
+			descp: "喜剧/动作",
+			actor: "易小星/彭昱畅/乔杉",
+			type1: "IMAX",
+			type2: "2D",
+			type3: "CINITY2D",
+			buy: "购票",
+			classType: "buyTypeOne"
+		},
+		{
+			id: 6,
+			img: "./img/cut_picture (9).jpg",
+			title: "送你一朵小红花",
+			classification: "9.1",
+			descp: "2020年12月31日上映",
+			actor: "韩延/易烊千玺/刘浩存",
+			type1: "IMAX",
+			type2: "2D",
+			type3: "CINITY2D",
+			buy: "预售",
+			classType: "buyTypeTwo"
+		},
+		{
+			id: 6,
+			img: "./img/cut_picture (21).jpg",
+			title: "明天你是否依然爱我",
+			classification: "9.1",
+			descp: "2020年12月24日上映",
+			actor: "周楠/杨颖/李鸿其",
+			type1: "IMAX",
+			type2: "2D",
+			type3: "CINITY2D",
+			buy: "预售",
+			classType: "buyTypeTwo"
+		}
+	]
+}
